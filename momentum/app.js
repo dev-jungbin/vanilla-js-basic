b2b57393ca46613f875cba2bb71a0259ff0823f4
dev-madilyn <lollin0@naver.com>
2022-03-20 19:31:40 +0900

    Chore: 상수, 변수, 콘솔, 연산

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,18 @@
-alert("hi");
+// 상수 선언
+const a = 6;
+const b = 2;
+
+// 변수 선언
+let myName = "madilyn";
+
+// 콘솔 찍기
+console.log(5423423);
+console.log('냐냐냐');
+
+// 연산, 변수 사용
+console.log(a + b);
+console.log(a * b);
+console.log("hello " + myName);
+
+myName = "jungbin"
+console.log("hi " + myName);
