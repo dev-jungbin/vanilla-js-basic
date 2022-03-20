7eef91d2e6a2806d13c618518d7f889e12ad5121
dev-madilyn <lollin0@naver.com>
2022-03-20 19:44:12 +0900

    Chore: Boolean, Null, Undefined

--- momentum/app.js
+++ momentum/app.js
@@ -5,6 +5,8 @@ const b = 2;
 // 변수 선언
 let myName = "madilyn";
 
+// var도 가능... 근데 옛날에만 쓰고 이제는 const/let으로 분리하여 사용
+
 // 콘솔 찍기
 console.log(5423423);
 console.log('냐냐냐');
@@ -16,3 +18,14 @@ console.log("hello " + myName);
 
 myName = "jungbin"
 console.log("hi " + myName);
+
+// Boolean
+const amIFat = false;
+console.log(amIFat);
+
+// Null
+const mySchool = null; // defined
+console.log("my school ", mySchool);
+
+let something; // undifined
+console.log("someting ", something);
